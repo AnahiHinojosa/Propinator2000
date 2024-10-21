1b5f0dd0369f74f507c38b3d04a71d422d908dcd
anahi hinojosa <ana11hinograje@gmail.com>
2024-10-21 14:01:27 +0200

    buttons

--- app/src/main/java/com/ahinojosa/propinator2000/MainActivity.java
+++ app/src/main/java/com/ahinojosa/propinator2000/MainActivity.java
@@ -1,9 +1,11 @@
 package com.ahinojosa.propinator2000;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,15 +16,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private Button[] buttons;
-    private RadioButton god,medium,bad;
+    private RadioButton selecionado;
     private TextView text;
     private TextView number;
     private Button calc;
+    private RadioGroup eleccion;
+    private int precio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        RadioGroup eleccion = findViewById(R.id.radios);
+        TextView number = findViewById(R.id.numeros);
+        Button calc = findViewById(R.id.calcular);
+        TextView text = findViewById(R.id.textprincipal);
         buttons = new Button[]{
                 findViewById(R.id.uno),
                 findViewById(R.id.dos),
@@ -35,14 +44,17 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.nueve),
                 findViewById(R.id.cero)
         };
-        RadioButton god = findViewById(R.id.buena);
-        RadioButton medium = findViewById(R.id.media);
-        RadioButton bad = findViewById(R.id.mala);
-        TextView number = findViewById(R.id.numeros);
-        Button calc = findViewById(R.id.calcular);
-        TextView text = findViewById(R.id.textprincipal);
 
+        for (Button i:buttons) {
+            i.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    precio+=i.getText().toString();
+                    total_precio.
+                }
+            });
 
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
